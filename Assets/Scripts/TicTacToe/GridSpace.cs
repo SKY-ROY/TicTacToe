61d44ccc36fb843ff9b7549b5844b8045ac9a90c
SKY <akash.rm.roy@gmail.com>
2020-03-21 02:43:10 +0530

    Create GridSpace.cs

---
+++ Assets/Scripts/TicTacToe/GridSpace.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttonText;
+    
+    private GameController_PvAI gameController;
+
+    public void SetSpace()
+    {
+        if (gameController.playerMove)                          //for player's turn
+        {
+            buttonText.text = gameController.GetPlayerSide();   //assigning a particular symbol from 'O' or 'X'
+            button.interactable = false;                        //cell interaction disabled after marking with any side
+            gameController.EndTurn();                           //check win conditions after every player turn
+        }
+    }
+    public void SetGameControllerReference(GameController_PvAI controller)
+    {
+        gameController = controller;
+    }
+}
