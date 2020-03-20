7dc9944e18b28a1e4ece9e596ba52649d0500ebb
SKY <akash.rm.roy@gmail.com>
2020-03-21 02:43:36 +0530

    Create StateData.cs

---
+++ Assets/Scripts/Save and Load/StateData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class StateData
+{
+    /// <summary>
+    /// data types to store various variable to handle GameState 
+    /// </summary>
+    public bool sfx;
+    public bool music;
+    public int qualityLevel;
+    public int xHighScore, oHighScore;
+    public int slotA_val, slotB_val, slotC_val;
+    public float volume;
+
+    public StateData(GlobalSettingsControl settings)
+    {
+        qualityLevel = settings.qualityLevelIndex;
+        music = settings.musicBool;
+        sfx = settings.sfxBool;
+        volume = settings.volumeVal;
+        xHighScore = settings.xHighScoreCount;
+        oHighScore = settings.oHighScoreCount;
+
+    }
+}
